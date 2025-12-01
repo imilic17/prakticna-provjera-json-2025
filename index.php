baf681edeef4d2cd9493a76583be8a7296c19ea0
Ecliptek <justdulexd@gmail.com>
2025-12-01 11:04:22 +0100

    submit forma verzija 1 dodan paknut pokusaj tablice u phpu

--- index.php
+++ index.php
@@ -5,34 +5,21 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>HTML5 Boilerplate</title>
-  <link rel="stylesheet" href="styles.css">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
 </head>
 
 <body>
+ 
 <?php 
 $predmetiString = file_get_contents(__DIR__.'/predmeti.json');
  $data =  json_decode($predmetiString, true);
 
-        if (count($data)) {
-            
-            echo "<table>";
 
-            foreach ($data as $stand) {
 
-                
-                echo "<tr>";
-                echo "<td>$id</td>";
-                echo "<td>$imepredmeta</td>";
-                echo "<td>$nazivprofesora</td>";
-                echo "<td>$godisnjifondsati</td>";
-                echo "<td>$preduvijet</td>";
-                echo "<td>$opis</td>";
-                echo "</tr>";
-            }
 
-          
-            echo "</table>";
-        }
+     
  
 
 
@@ -41,7 +28,25 @@ $predmetiString = file_get_contents(__DIR__.'/predmeti.json');
 
 
 ?>
-
+ <form>
+  <div class="form-group">
+    <input class="form-control" type="text" placeholder="Ime predmeta">
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="text" placeholder="Naziv profesora">
+  </div>
+   <div class="form-group">
+    <input class="form-control" type="text" placeholder="Godišnnji fond sati">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">je li predmet uvijet za sljedecu godinu</label>
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="text" placeholder="Opis predmeta">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 
 </html>
