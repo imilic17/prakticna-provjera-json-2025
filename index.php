91509d79db3bc15a3caa510a987064802d60057e
SoulStar1720 <sakarlena@gmail.com>
2025-12-01 10:16:08 +0100

    Dodan Bootstrep i prekopirana i modificirana forma za pretraživanje

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
   </head>
   <body>
     <?php
+    echo '<div class="container">
+        <div class="row">
+            <form class="row g-3">
+                
+                <div class="col-sm-3">
+                    <input type="text" name="predmet" class="form-control" id="preedmet">
+                </div>
 
+                <div class="col-sm-2">
+                    <button type="submit" class="btn btn-primary">Trazi</button>
+                </div>
+            </form>
+        </div>'
+        
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
