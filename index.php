49e119ab4ea04a1df53467a8fe9c888e26d48c86
Fran Rašić <fran.rasic1@ad.tsd.hr>
2025-12-02 10:16:36 +0100

    Dodana prazna tablica bez funkcionalnosti i php-a

--- index.php
+++ index.php
@@ -6,6 +6,33 @@
     <title>Index</title>
 </head>
 <body>
-    
+<div class='container'>
+        <div class="row">
+            <form class="row g-3">
+                
+                <div class="col-sm-3">
+                    <input placeholder="ime ososbe" type="text" name="osoba" class="form-control" id="osoba">
+                </div>
+
+                <div class="col-sm-3">
+                    <input placeholder="zadatak" type="text" name="zadatak" class="form-control" id="zadatak">
+                </div>
+
+                <div class="col-sm-2">
+                    <button type="submit" class="btn btn-primary">Trazi</button>
+                </div>
+            </form>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Ime i prezime</th>
+                        <th scope="col">Naziv zadatka</th>
+                        <th scope="col">Zadatak izvrsen</th>
+                    </tr>
+                </thead>
+                <tbody>
 </body>
 </html>
