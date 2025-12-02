3bebba52650d6803225bf21fe3e57f10bcc6b3cb
Teo Znamenaček <teo.znamenacek@ad.tsd.hr>
2025-12-02 10:19:35 +0100

    Dodavanje tablice i modal komponente

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+
+    $predmetString = file_get_contents(__DIR__.'/predmeti.json');
+    $predmeti = json_decode($predmetString, true);
+
+    
+
+
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadatak</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <div>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Novi korisnik
+        </button>
+    </div>
+
+
+
+
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    
+    
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Novi korisnik</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="unos_korisnika.php" method="POST">
+                    <div class="modal-body">
+                        <div class="mb-12">
+                            <label for="ime" class="form-label">Ime</label>
+                            <input type="text" class="form-control" name="ime" id="ime" placeholder="Ime">
+                        </div>
+                        <div class="mb-12">
+                            <label for="prezime" class="form-label">Prezime</label>
+                            <input type="text" class="form-control" name="prezime" id="prezime" placeholder="Prezime">
+                        </div>
+                        <div class="mb-12">
+                            <label for="datumRodenja" class="form-label">Datum rođenja</label>
+                            <input type="text" class="form-control" name="datumRodenja" id="datumRodenja" placeholder="Datum rođenja">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+                        <button type="submit" class="btn btn-primary">Spremi promjene</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
