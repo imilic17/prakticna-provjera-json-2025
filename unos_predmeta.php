7a38083d8acd1a4ca541cfaf57376891a91de6f7
Gordan Herceg <gordan.herceg@ad.tsd.hr>
2025-12-08 10:11:12 +0100

    Popravljen unos_predmeta i dodan automatski id

--- unos_predmeta.php
+++ unos_predmeta.php
@@ -1,21 +1,37 @@
 <?php
 
 $predmetiString = file_get_contents(__DIR__ . '/predmeti.json');
-$predmetiJson = json_decode($predmetiString);
+$predmetiJson = json_decode($predmetiString, true);
 
-    $predmet = array('id' => $_POST['id'],'NazivPredmeta' => $_POST['NazivPredmeta'],'ime' => $_POST['ime'], 'GodisnjiFondSati' => $_POST['GodisnjiFondSati'], 'UvjetGod' => $_POST['UvjetGod'],'OpisPredmeta' => $_POST['OpisPredmeta']);
-    if (isset($predmetiJson))
-    {
-        $predmetiJson[] = $predmet;
-    }
-    else
-    {
-        $predmetiJson = array($predmet);
-    }
 
-    $newString = json_encode($predmetiJson);
-    file_put_contents(__DIR__.'/predmeti.json', $newString);
+$nextId = 1; 
+if (!empty($predmetiJson)) {
+    $id1 = array_column($predmetiJson, 'id');
+    $nextId = max($id1) + 1;
+}
 
-    header("Location: http://localhost/Vjezba/prakticna-provjera-json-2025/index.php");
-    die();
+$predmet = array(
+    'id' => $nextId,
+    'NazivPredmeta' => $_POST['NazivPredmeta'],
+    'ime' => $_POST['ime'],
+    'GodisnjiFondSati' => $_POST['GodisnjiFondSati'],
+    'UvjetGod' => $_POST['UvjetGod'],
+    'OpisPredmeta' => $_POST['OpisPredmeta']
+);
+
+if (isset($predmetiJson))
+{
+    $predmetiJson[] = $predmet;
+}
+else
+{
+    $predmetiJson = array($predmet);
+}
+
+$newString = json_encode($predmetiJson);
+file_put_contents(__DIR__.'/predmeti.json', $newString);
+
+header("Location: http://localhost/Vjezba/prakticna-provjera-json-2025/index.php");
+die();
+
 ?>
