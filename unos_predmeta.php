ba308410ddf7626e28a65e2bcd38319fa99fe39e
Antonio Bičak <antonio.bicak1@ad.tsd.hr>
2025-12-08 09:54:43 +0100

    dodan unos_predmeta

---
+++ unos_predmeta.php
@@ -0,0 +1,22 @@
+<?php
+    $userString = file_get_contents(__DIR__.'/predmeti.json');
+    $usersData = json_decode($userString); 
+
+
+    $predmet = array('id' => $_POST['noviID'], 'naziv' => $_POST['naziv'], 'profesor' => $_POST['profesor'],
+     'fond_sati' => $_POST['fond_sati'], 'uvjet' => isset($_POST['uvjet']) ? 'DA' : 'NE',  'opis' => $_POST['opis']);
+    if (isset($usersData) && is_array($usersData))
+    {
+        $usersData[] = $predmet;
+    }
+    else
+    {
+        $usersData = array($predmet);
+    }
+
+    $newString = json_encode($usersData);
+    file_put_contents(__DIR__.'/predmeti.json', $newString);
+
+    header("Location: http://localhost/prakticna-provjera-json-2025/index.php");
+    die();
+?>
