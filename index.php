7bdb3b852472a1ad6ab3078daf99d362a928d2e0
Marko Lončarević <marko.loncarevic1@ad.tsd.hr>
2025-12-08 09:59:17 +0100

    napravljen izgled tablice

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+ 
+  </head>
+  <body>
+  <div class='container'>
+        <div class="row">
+            <form class="row g-3">
+                
+             
+
+                <div class="col-sm-3">
+                    <input  type="text" name="zadatak" class="form-control" id="zadatak">
+                </div>
+
+                <div class="col-sm-2">
+                    <button type="submit" class="btn btn-primary">Trazi predmet</button>
+                </div>
+            </form>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Naziv predmeta</th>
+                        <th scope="col">Ime profesora</th>
+                        <th scope="col">Godišnji fond sati</th>
+                        <th scope="col">Predmet je uvjet za iduću godinu</th>
+                        <th scope="col">Opis predmeta</th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                   
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+ 
+  </body>
+</html>
