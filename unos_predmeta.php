67f9939f08701053328ac70151ee9b1e2609a661
Teo Tuček <teo.tucek@ad.tsd.hr>
2025-12-09 10:16:41 +0100

    dodan unos predmeta i radi

---
+++ unos_predmeta.php
@@ -0,0 +1,29 @@
+<?php
+
+    $predmetString = file_get_contents(__DIR__.'/predmeti.json');
+    $predmetData = json_decode($predmetString);
+
+    if(empty($predmet)){
+        $newID=1;
+    }
+    else{
+        $last = end($predmeti);
+        $newID = $last["id"] + 1;
+    }
+
+    $predmet = array('id' => $newID,'naziv_predmeta' => $_POST['naziv_predmeta'], 'ime_profesora' => $_POST['ime_profesora'], 'godisnji_fond_sati' => $_POST['godisnji_fond_sati'], 'predmet_je_uvjet' => $_POST['predmet_je_uvjet'], 'opis_predmeta' => $_POST['opis_predmeta']);
+    if (isset($predmetData))
+    {
+        $predmetData[] = $predmet;
+    }
+    else
+    {
+        $predmetData = array($predmet);
+    }
+
+    $newString = json_encode($predmetData);
+    file_put_contents(__DIR__.'/predmeti.json', $newString);
+
+    header("Location: http://localhost/zadatak_s_unosom_korisnika/index.php");
+    die();
+?>
