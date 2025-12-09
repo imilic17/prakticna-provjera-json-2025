95518f2c3bd967de6cabad4ab2f463b43378f00a
Stjepan Patek <stjepan.patek@ad.tsd.hr>
2025-12-09 11:18:02 +0100

    Dodavanje modala

---
+++ zadatak_s_unosom_korisnika/index2.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Primjer 1!</title>
+  </head>
+  <body>
+    <div>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Novi korisnik
+        </button>
+    </div>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Ime</th>
+                <th scope="col">Prezime</th>
+                <th scope="col">Datum rođenja</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $userString = file_get_contents(__DIR__."/korisnici.json");
+                    $usersData = json_decode($userString, true);
+
+                    if (isset($usersData))
+                    {
+                        foreach ($usersData as $key => $value)
+                        {
+                            $ime = $value['ime'];
+                            $prezime = $value['prezime'];
+
+                            $datumRodenja = $value['datumRodenja'] ?? '';
+
+                            // $datumRodenja = (isset($value['datumRodenja'])) ? $value['datumRodenja'] : '';
+                            
+                            // $datumRodenja = '';
+                            // if (isset($value['datumRodenja']) )
+                            // {
+                            //     $datumRodenja = $value['datumRodenja'];
+                            // }
+                            
+    
+                            echo "<tr>
+                                <td>$ime</td>
+                                <td>$prezime</td>
+                                <td>$datumRodenja</td>
+                            </tr>";
+                        }
+                    }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Novi korisnik</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="unos_korisnika.php" method="POST">
+                    <div class="modal-body">
+                        <div class="mb-12">
+                            <label for="ime" class="form-label">Ime</label>
+                            <input type="text" class="form-control" name="ime" id="ime" placeholder="Ime">
+                        </div>
+                        <div class="mb-12">
+                            <label for="prezime" class="form-label">Prezime</label>
+                            <input type="text" class="form-control" name="prezime" id="prezime" placeholder="Prezime">
+                        </div>
+                        <div class="mb-12">
+                            <label for="datumRodenja" class="form-label">Datum rođenja</label>
+                            <input type="text" class="form-control" name="datumRodenja" id="datumRodenja" placeholder="Datum rođenja">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+                        <button type="submit" class="btn btn-primary">Spremi promjene</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
