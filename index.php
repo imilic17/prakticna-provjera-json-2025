d73c34793746a25c40a53a60a0c21abb20806351
Teo Tuček <teo.tucek@ad.tsd.hr>
2025-12-09 10:10:51 +0100

    dodao dugme za dodavanje predmeta i sve za dodaju predemeta

--- index.php
+++ index.php
@@ -89,5 +89,54 @@
                    
                 </tbody>
             </table>
+            </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Novi predmet</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="unos_predmeta.php" method="POST">
+                    <div class="modal-body">
+                  
+                        
+                        <div class="mb-12">
+                            <label for="naziv_predmeta" class="form-label">Naziv predmeta</label>
+                            <input type="text" class="form-control" name="naziv_predmeta" id="naziv_predmeta" placeholder="naziv predmeta">
+                        </div>
+                        <div class="mb-12">
+                            <label for="ime_profesora" class="form-label">Ime profesora</label>
+                            <input type="text" class="form-control" name="ime_profesora" id="ime_profesora" placeholder="ime profesora">
+                        </div>
+                        <div class="mb-12">
+                            <label for="godisnji_fond_sati" class="form-label">Godisnji fond sati</label>
+                            <input type="text" class="form-control" name="godisnji_fond_sati" id="godisnji_fond_sati" placeholder="fond sati koji se mora godisnje odraditi">
+                        </div>
+                        <div class="mb-12">
+                            <div class="from-check">
+                             <input type="checkbox" class="form-check-input" name="predmet_je_uvjet" id="predmet_je_uvjet" >
+                            <label for="predmet_je_uvjet" class="form-check-label">Predmet je uvjet za iducu godinu</label>
+                           
+                        </div>
+                        <div class="mb-12">
+                            <label for="opis_predmeta" class="form-label">Opis predmeta</label>
+                            <input type="text" class="form-control" name="opis_predmeta" id="opis_predmeta" placeholder="Opis">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zatvori</button>
+                        <button type="submit" class="btn btn-primary">Spremi promjene</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div> 
+    </div>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+ 
 </body>
 </html>
