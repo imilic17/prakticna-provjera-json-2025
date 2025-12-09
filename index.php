e9b00abec4e07008acc514e3962a2a287c262274
Teo Tuček <teo.tucek@ad.tsd.hr>
2025-12-09 10:02:45 +0100

    dodana kao tablica

--- index.php
+++ index.php
@@ -35,5 +35,59 @@
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
             Novi predmet
         </button> 
+        </form>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Naziv predmeta</th>
+                        <th scope="col">Ime profesora</th>
+                        <th scope="col">Godišnji fond sati</th>
+                        <th scope="col">Predmet je uvjet za iduću godinu</th>
+                        <th scope="col">Opis predmeta</th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    $predmetString = file_get_contents(__DIR__."/predmeti.json");
+                    $predmetData = json_decode($predmetString, true);
+
+                    if (isset($predmetData))
+                    {
+                        foreach ($predmetData as $key => $value)
+                        {
+                            $id = $value['id'];
+                            $naziv_predmeta = $value['naziv_predmeta'];
+                            $ime_profesora = $value['ime_profesora'];
+                            $godisnji_fond_sati = $value['godisnji_fond_sati'];
+
+                            $predmet_je_uvjet = $value['predmet_je_uvjet'];
+                            $opis_predmeta = $value['opis_predmeta'];
+
+
+                           
+
+                            echo "<tr>
+                                <td>$id</td>
+
+                                <td>$naziv_predmeta</td>
+                                <td>$ime_profesora</td>
+                                <td>$godisnji_fond_sati</td>
+                                <td>$predmet_je_uvjet</td>
+                                <td>$opis_predmeta</td>
+
+
+                            </tr>";
+                        }
+                    }
+                ?>
+               
+                   
+                </tbody>
+            </table>
 </body>
 </html>
