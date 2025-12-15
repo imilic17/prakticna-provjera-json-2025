315ab0a6e21e28875495166c3e4fe70125e7abf3
Thomas Vondra <vondrat7@gmail.com>
2025-12-15 10:24:04 +0100

    izbacio iz indexa sto je bilo visak

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<<<<<<< HEAD
                                                                                                            
   </head>
   <body>
@@ -118,7 +117,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-=======
+
 
     <title>Hello, world!</title>
   </head>
@@ -156,7 +155,5 @@
 </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
->>>>>>> 9ef70d6c943cb1b2ef315b2bd9ab923eb0419b02
   </body>
 </html>
