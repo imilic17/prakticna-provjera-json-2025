17bee685dc25173420271456d41881c4a61e2df0
Dorijan Hadžija <dorijan.hadzija@ad.tsd.hr>
2025-12-16 10:47:22 +0100

    FUNALNO

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
-        table.table tr.zeleni-red:nth-child(even),
-table.table tr.zeleni-red:hover {
-    background-color: #d4edda !important;
-}
-
+        .zeleni-red {
+            background-color: #d4edda !important;
+        }
+        
     </style>
     <title>Prakticna Provjera</title>
   </head>
@@ -27,9 +26,6 @@ table.table tr.zeleni-red:hover {
                     </div>
                     <div class="col-md-2">
                         <button type="submit" class="btn btn-primary w-100">Traži</button>
-                    </div>
-                    <div class="col-md-2">
-                        <a href="index.php" class="btn btn-secondary w-100">Očisti</a>
                     </div>
                 </form>
             </div>
@@ -65,17 +61,14 @@ table.table tr.zeleni-red:hover {
                         $pretraga = isset($_GET['pretraga']) ? strtolower($_GET['pretraga']) : '';
                         if (!empty($pretraga)){
                             $predmeti = array_filter($predmeti, function($predmet) use ($pretraga){
-                                return strpos(strtolower($predmet['ime']), $pretraga) !== false
-                                || strpos(strtolower($predmet['profesor']), $pretraga) !== false
-                                || strpos(strtolower($predmet['opis']), $pretraga) !== false;
+                                return strpos(strtolower($predmet['ime']), $pretraga) !== false;
                             });
                         }
                         if (empty($predmeti)) {
                             echo '<tr><td colspan="6" class="text-center">Nema podataka o predmetima.</td></tr>';
                         } else {
                             foreach ($predmeti as $predmet) {
-                                $klasa = (isset($predmet['uvjet']) && strtoupper(trim($predmet['uvjet'])) === 'DA') ? 'zeleni-red' : '';
-
+                                $klasa = ($predmet['uvjet'] === 'DA') ? 'table-success' : '';
                                 echo '<tr class="'. $klasa .'">';
                                 echo '<td>' . htmlspecialchars($predmet['id']) . '</td>';
                                 echo '<td>' . htmlspecialchars($predmet['ime']) . '</td>';
