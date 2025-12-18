d6640c5f239edd15eef5e3e287429718e948026d
Jakov123-maker <jakovpernar007@gmail.com>
2025-12-18 16:32:26 +0100

    dodao array predmet sa podatcima za tablicu

--- unos_predmeta.php
+++ unos_predmeta.php
@@ -10,7 +10,15 @@
     }
 
     $uvjet= isset($_POST['UvjetGod']) ? $_POST['UvjetGod'] :'NE';
-    
+
+    $predmet =array(
+        'id'=>$nextId,
+        'NazivPredmeta'=>$_POST['NazivPredmeta'],
+        'ime'=>$_POST['ime'],
+        'GodisnjiFondSati'=>$_POST['GodisnjiFondSati'],
+        'UvjetGod'=>$uvjet,
+        'OpisPredmeta'=>$_POST['OpisPredmeta']
+    );
 
     $newString = json_encode($usersData);
     file_put_contents(__DIR__.'/predmeti.json', $newString);
