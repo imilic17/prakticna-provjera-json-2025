9152aa5aa564108d4ef91b5e811e7fa6b6f85799
Jakov123-maker <jakovpernar007@gmail.com>
2025-12-18 16:29:11 +0100

    uvjet

--- unos_predmeta.php
+++ unos_predmeta.php
@@ -9,15 +9,8 @@
         $nextId = max($ids)+1;
     }
 
-    $user = array('ime_profesora' => $_POST['ime'], 'fond_sati' => $_POST['fond_sati'], 'naziv_predmeta' => $_POST['naziv_predmeta'], 'uvjet_za_sljedecu_godinu' => $POST['uvjet_za_sljedecu_godinu']);
-    if (isset($usersData))
-    {
-        $usersData[] = $user;
-    }
-    else
-    {
-        $usersData = array($user);
-    }
+    $uvjet= isset($_POST['UvjetGod']) ? $_POST['UvjetGod'] :'NE';
+    
 
     $newString = json_encode($usersData);
     file_put_contents(__DIR__.'/predmeti.json', $newString);
