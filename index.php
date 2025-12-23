17eb2da1fe7b80c4894b827849ce0bd0c56be63f
Borna Sertić <borna.sertic2@ad.tsd.hr>
2025-12-23 09:47:12 +0100

    pocetni commit 23.12.

--- index.php
+++ index.php
@@ -8,11 +8,35 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Tablica Borna</title>
   </head>
   <body>
   <table class="table">
   <thead>
+
+  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <tr>
       <th scope="col">ID</th>
       <th scope="col">Naziv Predmeta</th>
@@ -32,11 +56,11 @@
                         foreach ($usersData as $key => $value)
                         {
                             $NazivPredmeta = $value['NazivPredmeta'];
-                            $ImeProfesora = $value['ImeProfedora'];
+                                   $ImeProfesora = $value['ImeProfesora'];
 
                             $GodFondSat = $value['GodFondSat'];
-                            $PJUZIG = $value['PJUZIG'];
-                            $OpisPredmeta = $value['OpisPredmeta'];
+                            $PJUZIG = $value['PJUZIG'] ?? " ";
+                            $OpisPredmeta = $value['OpisPredmeta']?? " ";
 
                            
                             echo "<tr>
